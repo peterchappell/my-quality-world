3ff4c6076ca5b664b90e4ea4510a3f3f85907987
Pete Chappell <pchappell@intellum.com>
2020-07-08 16:42:51 +0100

    Add some needs met charts to the home page.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+
+import NeedsChart from 'components/NeedsChart';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -12,7 +15,33 @@ const useStyles = makeStyles((theme) => ({
     margin: [[theme.spacing(2), 0]],
     textAlign: 'center',
   },
+  needsContainer: {
+    marginTop: theme.spacing(2),
+  },
 }));
+
+const NEEDS = [
+  {
+    name: 'Physiology',
+    key: 'physiology',
+  },
+  {
+    name: 'Love and Belonging',
+    key: 'loveAndBelonging',
+  },
+  {
+    name: 'Power',
+    key: 'power',
+  },
+  {
+    name: 'Fun',
+    key: 'fun',
+  },
+  {
+    name: 'Freedom',
+    key: 'freedom',
+  },
+];
 
 const Home = (props) => {
   const {
@@ -20,6 +49,18 @@ const Home = (props) => {
   } = props;
 
   const classes = useStyles();
+
+  const calculateNeedMetPercent = (need) => {
+    const itemsCheck = [...items];
+    const maxForNeed = itemsCheck.length * 5 * 11;
+    let needScore = 0;
+    itemsCheck.forEach((item) => {
+      needScore += item[need] * item.level;
+    });
+    // const needScore = itemsCheck.reduce((accumulator, item) => item[need] * item.level);
+    console.log('need', need, maxForNeed, needScore, (needScore / maxForNeed) * 100);
+    return (needScore / maxForNeed) * 100;
+  };
 
   return (
     <div className={classes.container}>
@@ -31,6 +72,16 @@ const Home = (props) => {
         {` ${items.length} `}
         items in your quality world.
       </Typography>
+      <Typography variant="body1" component="p" gutterBottom>
+        Here is an overview of how your quality world is meeting your needs.
+      </Typography>
+      <Grid container spacing={3} className={classes.needsContainer}>
+        { NEEDS.map((need) => (
+          <Grid item xs key={`need_score_${need.key}`}>
+            <NeedsChart value={calculateNeedMetPercent(need.key)} label={need.name} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
---
+++ src/components/NeedsChart/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  circleContainer: {
+    position: 'relative',
+  },
+  background: {
+    color: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+  },
+  value: {
+    animationDuration: '550ms',
+    position: 'absolute',
+    left: 0,
+  },
+  valueCircle: {
+    strokeLinecap: 'round',
+  },
+  outerContainer: {
+    display: 'inline-flex',
+  },
+  label: {
+    display: 'block',
+  },
+}));
+
+const NeedsChart = (props) => {
+  const {
+    value,
+    label,
+  } = props;
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <Box position="relative" className={classes.outerContainer}>
+        <div className={classes.circleContainer}>
+          <CircularProgress
+            variant="static"
+            value={100}
+            size={60}
+            thickness={4}
+            className={classes.background}
+          />
+          <CircularProgress
+            variant="static"
+            value={value}
+            size={60}
+            thickness={4}
+            className={classes.value}
+            classes={{
+              circle: classes.valueCircle,
+            }}
+          />
+        </div>
+        <Box
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          position="absolute"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="caption" component="div" color="textSecondary">
+            {`${Math.round(value)}%`}
+          </Typography>
+        </Box>
+      </Box>
+      <Typography variant="caption" className={classes.label}>
+        {label}
+      </Typography>
+    </>
+  );
+};
+
+export default NeedsChart;
