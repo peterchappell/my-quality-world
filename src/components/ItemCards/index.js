27ece133bc309a767f60ecdf57a4035cdbb66f47
Pete Chappell <pchappell@intellum.com>
2020-07-10 18:14:50 +0100

    Add previous next buttons to the carousel.

--- src/components/ItemCards/index.js
+++ src/components/ItemCards/index.js
@@ -4,14 +4,42 @@ import { virtualize } from 'react-swipeable-views-utils';
 import { mod } from 'react-swipeable-views-core';
 
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 import ItemCard from 'components/ItemCard';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   mainContainer: {
     alignItems: 'center',
     display: 'flex',
     margin: [[0, 'auto']],
+  },
+  previousButton: {
+    background: 'rgba(255,255,255,0.7)',
+    borderRadius: '50%',
+    left: 0,
+    position: 'absolute',
+    top: '100px',
+    zIndex: 10,
+    [theme.breakpoints.up('sm')]: {
+      top: '200px',
+    },
+  },
+  nextButton: {
+    background: 'rgba(255,255,255,0.7)',
+    borderRadius: '50%',
+    position: 'absolute',
+    right: 0,
+    top: '100px',
+    zIndex: 10,
+    [theme.breakpoints.up('sm')]: {
+      top: '200px',
+    },
+  },
+  buttonHide: {
+    display: 'none',
   },
 }));
 
@@ -25,6 +53,7 @@ const ItemCards = (props) => {
 
   const classes = useStyles();
   const [currentIndex, setCurrentIndex] = useState(itemIndex);
+  const [isSliding, setIsSliding] = useState(false);
 
   const updateIndex = (newIndex) => {
     setCurrentIndex(newIndex);
@@ -44,8 +73,36 @@ const ItemCards = (props) => {
         index={currentIndex}
         onChangeIndex={updateIndex}
         enableMouseEvents
+        onSwitching={() => {
+          setIsSliding(true);
+        }}
+        onTransitionEnd={() => {
+          setIsSliding(false);
+        }}
         slideRenderer={slideRenderer}
       />
+      <div className={isSliding ? classes.buttonHide : classes.previousButton}>
+        <IconButton
+          aria-label="Previous"
+          color="primary"
+          onClick={() => {
+            setCurrentIndex(currentIndex - 1);
+          }}
+        >
+          <KeyboardArrowLeftIcon />
+        </IconButton>
+      </div>
+      <div className={isSliding ? classes.buttonHide : classes.nextButton}>
+        <IconButton
+          aria-label="Next"
+          color="primary"
+          onClick={() => {
+            setCurrentIndex(currentIndex + 1);
+          }}
+        >
+          <KeyboardArrowRightIcon />
+        </IconButton>
+      </div>
     </div>
   );
 };
